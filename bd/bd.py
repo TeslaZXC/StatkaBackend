e20774e50f67b8c754f6a94579524a8655bb26bd
TeslaQ1 <104022375+TeslaZXC@users.noreply.github.com>
2025-09-09 23:00:09 +0300

    new

--- bd/bd.py
+++ bd/bd.py
@@ -7,11 +7,12 @@ missions = db["mission_stat"]
 def get_all_missions():
     data = missions.find({}, {
         "_id": 0,
-        "id": 1,               
+        "id": 1,                
         "file": 1,
         "file_date": 1,
         "game_type": 1,
         "duration_frames": 1,
+        "duration_time": 1,  
         "missionName": 1,
         "worldName": 1,
         "win_side": 1
@@ -26,6 +27,7 @@ def get_mission_info(mission_id: int):
         "file_date": 1,
         "game_type": 1,
         "duration_frames": 1,
+        "duration_time": 1,  
         "missionName": 1,
         "worldName": 1,
         "win_side": 1
